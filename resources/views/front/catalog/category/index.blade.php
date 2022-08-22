3c24c9c4fa54002290a250e13e1519d81d41ca05
Filip Jankoski <filip@agmedia.hr>
2022-08-22 20:31:20 +0200

    Show author description.

--- resources/views/front/catalog/category/index.blade.php
+++ resources/views/front/catalog/category/index.blade.php
@@ -161,12 +161,19 @@
 
         {{--        </div>--}}
     </div>
-    <div class="container pb-4 mb-2 mb-md-4" >
-    @if ($cat && ! $subcat)
-        {!! $cat->description !!}
-    @elseif ($subcat && ! $subcat)
-        {!! $cat->description !!}
+
+    @if (isset($author) && $author && ! empty($author->description))
+        <div class="container pb-4 mb-2 mb-md-4" >
+            {!! $author->description !!}
+        </div>
     @endif
+
+    <div class="container pb-4 mb-2 mb-md-4" >
+        @if ($cat && ! $subcat)
+            {!! $cat->description !!}
+        @elseif ($subcat && ! $subcat)
+            {!! $cat->description !!}
+        @endif
     </div>
 
 @endsection
