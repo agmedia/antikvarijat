a9792b64587c6e275ebd782cb5f254f7733d2589
Filip Jankoski <filip@agmedia.hr>
2022-09-01 09:46:41 +0200

    #Fix: single product discount price... widgets.

--- resources/views/front/catalog/category/product.blade.php
+++ resources/views/front/catalog/category/product.blade.php
@@ -30,7 +30,7 @@
         <div class="d-flex flex-wrap justify-content-between align-items-center price-box mt-2">
             @if ($product->special())
                 <div class="bg-faded-accent text-accent text-sm rounded-1 py-1 px-1" style="text-decoration: line-through;">{!! $product->priceString() !!}</div>
-                <div class="bg-faded-accent text-accent text-sm rounded-1 py-1 px-1">{!! $product->priceString($product->special()) !!}</div>
+                <div class="bg-faded-accent text-accent text-sm rounded-1 py-1 px-1">{{ number_format(\App\Helpers\Helper::calculateDiscountPrice($product->price, floatval(\App\Helpers\Helper::calculateDiscount($product->price, $product->special()))), 2, ',', '.') }}</div>
             @else
                 <div class="bg-faded-accent text-accent rounded-1 py-1 px-1">{!! $product->priceString() !!}</div>
             @endif
