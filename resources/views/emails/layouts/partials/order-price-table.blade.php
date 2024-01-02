2a24b187b28e90194a84a833336ef5a71e86317a
Filip Jankoski <filip@agmedia.hr>
2024-01-02 09:33:00 +0100

    _fix: Mail € view.

--- resources/views/emails/layouts/partials/order-price-table.blade.php
+++ resources/views/emails/layouts/partials/order-price-table.blade.php
@@ -43,8 +43,8 @@
         <tr>
             <td>{{ $product->name }} - {{ $product->product->sku}}</td>
             <td style="text-align: center;">{{ $product->quantity }}</td>
-            <td style="text-align: right;">{{ number_format($product->price, 2, ',', '.') }}</td>
-            <td style="text-align: right;">{{ number_format($product->total, 2, ',', '.') }}</td>
+            <td style="text-align: right;">€ {{ number_format($product->price, 2, ',', '.') }}</td>
+            <td style="text-align: right;">€ {{ number_format($product->total, 2, ',', '.') }}</td>
         </tr>
     @endforeach
 </table>
@@ -57,7 +57,7 @@
             @if ($order->shipping_state != 'Croatia' && $total->code == 'shipping')
                 <td style="border-left: none; text-align: right; {{ $total->code == 'shipping' ? '' : 'font-weight: bold;' }}" width="20%">Trošak dostave će Vam biti poslan u roku od 24h.</td>
             @else
-                <td style="border-left: none; text-align: right; {{ $total->code == 'shipping' ? '' : 'font-weight: bold;' }}" width="20%">{{ number_format($total->value, 2, ',', '.') }}</td>
+                <td style="border-left: none; text-align: right; {{ $total->code == 'shipping' ? '' : 'font-weight: bold;' }}" width="20%">€ {{ number_format($total->value, 2, ',', '.') }}</td>
             @endif
         </tr>
     @endforeach
@@ -66,9 +66,9 @@
 <p style="text-align: right;font-size: 10px;"> PDV uključen u cijenu. Od toga
     @foreach ($order->totals as $total)
         @if($total->code == 'subtotal')
-        €<strong>{{ number_format($total->value - ($total->value / 1.05 ), 2, ',', '.') }}</strong>  PDV knjige i
+        € <strong>{{ number_format($total->value - ($total->value / 1.05 ), 2, ',', '.') }}</strong>  PDV knjige i
     @elseif ($total->code == 'shipping')
-        €<strong>{{number_format($total->value - ($total->value / 1.25 ), 2, ',', '.') }}</strong>  PDV dostava
+        € <strong>{{number_format($total->value - ($total->value / 1.25 ), 2, ',', '.') }}</strong>  PDV dostava
     @endif
     @endforeach
 </p>
