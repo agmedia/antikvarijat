d5aa4a779c29c51f0229552bf92083c9cc6ab8c6
tomashina <tomislav@agmedia.hr>
2024-01-18 15:19:25 +0100

    helper youtube

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -437,7 +437,7 @@ class Helper
         $text = preg_replace('/<oembed.*?<\/oembed>/i','', $text);
         $has = [$text, $matches[0]];
 
-        $text = str_replace('<figure class="media"></figure>', '<iframe width="560" height="315" src="' . $has[1][0] . '" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>', $text);
+        $text = str_replace('<figure class="media"></figure>', '<iframe width="100%" height="450" src="' . $has[1][0] . '" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>', $text);
 
         return $text;
     }
