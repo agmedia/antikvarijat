2bb7ca9de192d122d9e7d23c62230909d2ddcb3b
tomashina <tomislav@agmedia.hr>
2025-09-19 12:07:55 +0200

    komentar

--- resources/views/livewire/front/checkout.blade.php
+++ resources/views/livewire/front/checkout.blade.php
@@ -200,15 +200,7 @@
         </div>
 
 
-            <h2 class="h6 pt-1 pb-3 mb-3 border-bottom">Dodatna napomena</h2>
-            <div class="row mt-3">
-                <div class="col-sm-12">
-                    <div class="mb-3">
-                        <label class="form-label" for="checkout-comment">Komentar</label>
-                        <textarea class="form-control" id="checkout-comment" rows="3" wire:model="comment"></textarea>
-                    </div>
-                </div>
-            </div>
+
 
         <h2 class="h6 pt-1 pb-3 mb-3 border-bottom">Trebate R1 račun?</h2>
         <div class="row mt-3">
