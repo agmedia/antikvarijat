9aa8e9de95a19b4d1b462d9fb723a132c31d679f
tomashina <tomislav@agmedia.hr>
2025-11-13 09:53:15 +0100

    steps fix

--- resources/views/front/checkout/cart.blade.php
+++ resources/views/front/checkout/cart.blade.php
@@ -53,7 +53,11 @@
                 </a>
                 <a class="step-item" href="{{ route('pregled') }}">
                     <div class="step-progress"><span class="step-count">5</span></div>
-                    <div class="step-label"><i class="ci-check-circle"></i>Pregledaj</div>
+                    <div class="step-label"><i class="ci-eye"></i>Pregledaj</div>
+                </a>
+                <a class="step-item" href="#">
+                    <div class="step-progress"><span class="step-count">6</span></div>
+                    <div class="step-label"><i class="ci-check-circle"></i>Uspješno</div>
                 </a>
             </div>
             <div class="bg-white rounded-3 shadow-lg p-4">
